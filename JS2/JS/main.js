f6701e374cd70c1d57f85eeb189f6bccf92dc5b4
Hellstaff57 <56507745+Hellstaff57@users.noreply.github.com>
2020-03-29 00:30:38 +0200

    Add files via upload

--- JS2/JS/main.js
+++ JS2/JS/main.js
@@ -1,50 +1,28 @@
-//1
-// let k1='5px',
-//     k2='12djd334',
-//     k3='12.45asdwe12',
-//     k4='qwqweeewq';
-// console.log(Number.parseInt(k1));
-// console.log(Number.parseInt(k2));
-// console.log(Number.parseInt(k3));
-// console.log(Number.parseInt(k4));
+// let k1 = '5px',
+//    k2 = '12seghurhguierj334',
+//    k3 = '12 .666feyfghuieghui14',
+//    k4 = 'qwertyuiop';
+// console.log (Number.parseInt (k1));
+//             (Number.parseInt (k2));
+//             (Number.parseInt (k3));
+//             (Number.parseInt (k4));
 
-//2
-// let k1="5px",
-//     k2="12djd334",
-//     k3="12.45asdwe12",
-//     k4="qwqweeewq";
-// console.log(Number.parseFloat(k1));
-// console.log(Number.parseFloat(k2));
-// console.log(Number.parseFloat(k3));
-// console.log(Number.parseFloat(k4));
 
-//3
-// let random = Math.floor(Math.random() * 10) + 1;
-// console.log(Math.pow(random, 2));
+// let k1 = '5px',
+//    k2 = '12seghurhguierj334',
+//    k3 = '12 feyfghuieghui.14',
+//    k4 = 'qwertyuiop';
+// console.log (Number.parseFloat (k1));
+//          (Number.parseFloat (k2));
+//          (Number.parseFloat (k3));
+//          (Number.parseFloat (k4));
+  
 
-//4
-// let n1='My',
-//     n2='name',
-//     n3='is',
-//     fullName= `${n1} ${n2} ${n3} Viktor.`;
-// console.log(fullName);
+// let random = Math.floor (Math.random () * (10-1) +1); 
+// console.log (Math.pow (random, 2));
 
-//5
-// let result = 5+5+'5';
-// console.log(typeof result);
 
-//6
-// let num=20;
-// const result = num > 10 && num < 30;
-// console.log(result);
 
-//7
-// let num=40;
-// const result = num < 10 || num > 30;
-// console.log(result);
-
-//8
-// console.log(!true);
-// console.log(!false);
-// console.log(!1);
-// console.log(!0);
+// const a = 'My',
+// b = 'name',
+// c = 'is';
