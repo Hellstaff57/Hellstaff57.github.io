cd52c3d43386c756bf271941c6e2b944c447aee9
Hellstaff57 <56507745+Hellstaff57@users.noreply.github.com>
2020-03-28 14:05:18 +0200

    Add files via upload

--- JS1/JS/main.js
+++ JS1/JS/main.js
@@ -1,29 +1,27 @@
-//1 номер
-// let str = "Привіт";
-// let num = 123;
-// let flag = true;
-// let txt = "true";
+// let str = "добрий день";
+//     num = 123;
+//     flag = true;
+//     txt = "true";
 // console.log(typeof(str));
-// console.log(typeof(num));
-// console.log(typeof(flag));
-// console.log(typeof(txt));
+//            (typeof(num));
+//            (typeof(flag));
+//            (typeof(txt));
 
-//2 номер
-// let str = "Вітя - ТОП",
-//     flag = false,
-//     obj = {},
-//     number = 1337,
-//     array2 = [1,2,3];
-// const num = 1441,
-//     txt = "false",
-//     array1 = ["hello"],
-//     string = "Купи слона!",
-//     point = true;
+// let str = "кабзда";
+//     num = 123;
+//     flag = false;
+//     txt = "жесть";
+//     obj = {};
+// const array1 = [HELLO];
+//       array2 = ["123"];
+//       string = "фільм місто кісток топчик",
+//       number = 1234124;
+//       boolean = "відео";
 
-//3 номер
-// let myAge = 15,
-//     kolyaAge = 14,
-//     sum = myAge+kolyaAge;
+
+// let myAge = 14,
+//     Age = ???,
+//     sum = myAge+Age;
 // alert(sum);
 // let n1 = 108,
 //     n2 = 223,
@@ -32,59 +30,3 @@
 //     result = n1+n2-n3*n4;
 // console.log(result);
 
-//4 номер
-// let a1 = 5+3,
-//     a2 = 5-3,
-//     a3 = 5*3,
-//     a4 = 5/3,
-//     a5 = 5**3;
-// console.log(a1);
-// console.log(a2);
-// console.log(a3);
-// console.log(a4);
-// console.log(a5);
-
-//5 номер
-// let a6 = 5%3,
-//     a7 = 3%5,
-//     a8 = 5+'3',
-//     a9 = '5'-3,
-//     a10 = 75+'кг';
-// alert(a6);
-// alert(a7);
-// alert(a8);
-// alert(a9);
-// alert(a10);
-
-//6 номер
-// let width = 10,
-//     heiht = 23,
-//     s = width*heiht;
-// alert(s);
-
-//7 номер
-// const p = 3.14;
-// let heightC = 10,
-//     dC = 4,
-//     rC = dC/2,
-//     v = p*rC**2*heightC;
-// alert(v);
-
-//8 номер
-// let n = 3,
-//     m = 4,
-//     k = Math.sqrt(Math.pow(3, 2) + Math.pow(4, 2));
-// alert(k);
-
-//8 номер(з сайту)
-// let s = 2000,
-//     p = 10,
-//     years = 5,
-//     perepl = (s/100*p)*years;
-// alert(perepl);
-
-//7 номер(з сайту)
-// let nFib = 12,
-//     el = 12,
-//     fn = nFib*el;
-// alert(fn);
