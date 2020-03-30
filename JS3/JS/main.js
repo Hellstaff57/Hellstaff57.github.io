2f5cdef0ece49ca5bb84c1189fa1c690e02e28e4
Hellstaff57 <56507745+Hellstaff57@users.noreply.github.com>
2020-03-30 16:03:04 +0300

    Add files via upload

---
+++ JS3/JS/main.js
@@ -0,0 +1,21 @@
+//  const data = 1;
+// if (data==1) {
+//      console.log("я робив сам дз!");
+//  }   else {
+//      console.log("29.03.2020: ", data);
+//  } 
+
+//  const data = 1;
+//  if (data%2==0)  {
+//      console.log(data);
+//  }  else {
+//      console.log(" того так мало вибач!!!");
+//  } 
+
+//  let a = 5%2;
+
+//  if (a==0)  {
+//      console.log(5%2 + " компютер");
+//  }  else  {
+//      console.log(a + " ноутбук");
+//  }
