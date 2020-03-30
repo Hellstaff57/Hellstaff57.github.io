10656500ddb27d8fe2ef4ed3c076eacd68df28c0
Hellstaff57 <56507745+Hellstaff57@users.noreply.github.com>
2020-03-30 16:09:24 +0300

    Add files via upload

---
+++ JS4/JS/main.js
@@ -0,0 +1,23 @@
+// for (let i=0;i<11;i++)  {
+// console.log(i);
+// }
+
+// for (let i=1;i<11;i++)  {
+//   if (i%2===0) {
+//       console.log("парне число");
+//   }  else {
+//       console.log("не перне число");
+//    } 
+// }
+
+//  let num=20;
+//  while (num<29)  {
+//      console.log(num);
+//      num++;
+// }
+
+//  let name= prompt("мого кота звати Марсик: ");
+//  while (name.length<6) {
+//      console.log(name);
+//  let name = prompt("мого кота звати Марсик: ");
+// }
