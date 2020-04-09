9edff34a15534d8c68dcf7620be8562ad3581be5
Hellstaff57 <56507745+Hellstaff57@users.noreply.github.com>
2020-04-09 11:32:02 +0300

    Add files via upload

---
+++ JS6/JS/main.js
@@ -0,0 +1,25 @@
+//   function hi1(  )  {
+//       return console.log('main.js ето очень сложно!!!!!!!!!!!') ;
+//   } 
+//   hi1 () ;
+
+//   function hi2(name) {
+//     return  console.log('как дела???,', neme) ;
+//   }
+//   hi2('Савлук ') ;
+
+//   function mull(n,m) {
+//     return console.log(n+m,n*m,n-m) ;
+//   }
+//   mull(3, 2);
+
+//   function repeat(str,n=2)  {
+//         for(let  i=0;i<n;i++)  {
+//           console.log(str) ;
+//       }
+//   }
+//   repeat(' ', 2) ;
+
+//   function  avg() {
+//       let average = 0,
+//           sum = 0,
