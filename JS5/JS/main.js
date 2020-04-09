02e6cc119dd07678207700d539c6a5446b96a326
Hellstaff57 <56507745+Hellstaff57@users.noreply.github.com>
2020-04-09 11:31:23 +0300

    Add files via upload

---
+++ JS5/JS/main.js
@@ -0,0 +1,29 @@
+//  let Arr1=[1, 5, "4", "Hi"],
+//        Arr2=[true,2,{},["a"],222];
+//  console.log(Arr1[1]);
+//  console.log(Arr2[1]);
+//  console.log(Arr1[1]+Arr2[1]);
+
+//  let Arr1=[1, 5, "4", "Hi"];
+//  Arr1[4]=22;
+//  for (let i=0;i<Arr1.length; i++)  {
+///        coтsole.log(Arr1[i]);
+// }
+
+// let Arr2=[true,2,{},["a"],222];
+// for (const character of Arr2) {
+//       console.log(character,typeof(character));
+// }
+
+//  const matrix=[ [10,11,12], [13,14,15,], [16,17,18] ];
+//  for (let i=0; i<matrix.length; i++) {
+//        console.log(matrix[i] );
+//        for (let j=0; j<matrix[i].length; j++)  {
+//        console.log(matrix[i] [j] );
+//      }
+//  }
+
+//  const message='Вітаю у Дома,привет Вітя,як справи?';
+//  let arr=message.split('');
+//        console.log(arr);
+///        console.log(arr.indexOf('1') );
