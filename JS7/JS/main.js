d2f360cb61588766a2525fe7e947ecde6aeb8e78
Hellstaff57 <56507745+Hellstaff57@users.noreply.github.com>
2020-05-11 14:47:42 +0300

    Add files via upload

---
+++ JS7/JS/main.js
@@ -0,0 +1,59 @@
+//1
+
+
+// let obj = {
+//     name: "artur",
+//     age: 14,
+//     sex: "Male",
+//     married:
+// };
+
+//2
+// let obj = {
+//     name: "artur",
+//     age: 14,
+//     sex: "Male",
+//     married: 
+// };
+// delete obj.sex;
+// obj.language = "JS";
+
+
+// let numbers = { };
+// function num() {
+//     for (let i=1; i<=10; i++) {
+//     if (i%2 === 0) {
+//     numbers[i] = i;
+//         }
+//     }
+// }
+// num();
+// console.log(numbers);
+
+
+//3
+
+
+// let obj = {
+//     first: 1,
+//     second: 2
+// };
+// obj.sum = function() {
+//     console.log(obj.first + obj.second);
+// };
+// obj.sum();
+
+
+//4
+
+
+// let obj = {
+//     name: "artur",
+//     age: 14,
+//     sex: "Male",
+//     married: 
+// };
+// let keys = Object.keys(obj);
+// for (let i=0; i<keys.length; i++) {
+//     console.log('keys:', keys[i]);
+// }
